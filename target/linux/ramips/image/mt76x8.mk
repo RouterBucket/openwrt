de1177958b04173ad8fa73d6e526207a3c2ff7c8
Gustavo Mathias Rocha <gustavo.mathias.rocha@gmail.com>
2023-09-13 16:31:44 -0300

    Update mt76x8.mk
    Signed-off-by: Gustavo Mathias Rocha <gustavo.mathias.rocha@gmail.com>

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -795,7 +795,8 @@ define Device/tplink_tl-wr841hp-v5
   TPLINK_HWREVADD := 0x5
   IMAGES := sysupgrade.bin tftp-recovery.bin
   IMAGE/tftp-recovery.bin := pad-extra 128k | $$(IMAGE/factory.bin)
-  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ehci kmod-usb-ohci kmod-usb-ledtrig-usbport
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ehci kmod-usb-ohci kmod-usb-ledtrig-usbport \
+         kmod-usb-storage kmod-fs-ext4 block-mount
   SUPPORTED_DEVICES += tl-wr841hp-v5
 endef
 TARGET_DEVICES += tplink_tl-wr841hp-v5
